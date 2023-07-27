a4f785cc6e136d2f76a40385828a10c85cabbe32
jin3811 <hjwoo1028@naver.com>
2023-07-28 05:17:41 +0900

    2023-07-26

---
+++ 27866.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+    string str;
+    int n;
+    cin >> str >> n;
+    cout << str[n - 1];
+}
