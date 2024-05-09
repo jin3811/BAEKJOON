c7a980f67c84436b7af14c0a45db3632f82eefb4
jin3811 <hjwoo1028@naver.com>
2024-05-10 03:17:18 +0900

    24.05.09

--- 10854.cpp
+++ 10854.cpp
@@ -1,19 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 using ll = int64_t;
 using ull = uint64_t;
 
 struct Random {
 	mt19937 rd;
-	Random() : rd((unsigned)chrono::steady_clock::now().time_since_epoch().count()) {}
-	Random(int seed) : rd(seed) {}
+	Random(int seed = (unsigned)chrono::steady_clock::now().time_since_epoch().count()) : rd(seed) {}
 	template<typename T = int>
 	T GetInt(T l = 0, T r = 32767) {
 		return uniform_int_distribution<T>(l, r)(rd);
-	}
-	double GetDouble(double l = 0, double r = 1) {
-		return uniform_real_distribution<double>(l, r)(rd);
 	}
 } Rand;
 
@@ -90,6 +85,7 @@ struct PollardRho : public MillerRabin {
 
 
 int main() {
+	cin.tie(0)->sync_with_stdio(0);
 	ll n, ans=1; cin >> n;
 	map<ll, int> fac;
     for (auto& i : P.Factorize(n)) fac[i]++;
