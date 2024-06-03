9c42f4f7690b2ae8ff3832741f272a731806a41b
jin3811 <hjwoo1028@naver.com>
2024-06-03 13:30:26 +0900

    24.06.03

---
+++ 11726.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define mod 10007
+using namespace std;
+
+int n;
+
+void sol() {
+	int dp[n + 1] = {1, 1};
+	for (int i = 2; i <= n; i++) {
+		dp[i] = (dp[i - 2] + dp[i - 1]) % mod;
+	}
+	cout << dp[n];
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(false);
+	cin >> n;
+	sol();
+}
