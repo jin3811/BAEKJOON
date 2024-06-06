4fea4cb1784697474fa90c2e1d8f82fc383f1ec0
jin3811 <hjwoo1028@naver.com>
2024-06-06 15:47:03 +0900

    24.05.25

--- 14940.cpp
+++ 14940.cpp
@@ -8,75 +8,58 @@ vector<vector<int>> board;
 pii goal;
 
 void input() {
-    cin >> n >> m;
-    board.resize(n);
+	int road;
+	cin >> n >> m;
+	board.resize(n);
 
-    for(int y = 0; y < n; y++) {
-        board[y].resize(m);
+	for(int y = 0; y < n; y++) {
+		board[y].resize(m);
 
-        for(int x = 0; x < m; x++) {
-            cin >> board[y][x];
-            if (board[y][x] == 2) {
-                goal.first = x;
-                goal.second = y;
-            }
-        }
-    }
+		for(int x = 0; x < m; x++) {
+			cin >> road;
+			if (road == 2) {
+				goal.first = x;
+				goal.second = y;
+			}
+			if (road > 0) {
+				board[y][x] = -1;
+			}
+		}
+	}
 }
 
 void sol() {
-    queue<pii> q;
-    int dx[4] = {1, 0, -1, 0};
-    int dy[4] = {0, 1, 0, -1};
+	queue<pii> q;
+	int dx[4] = {1, 0, -1, 0};
+	int dy[4] = {0, 1, 0, -1};
 
-    int now = 0;
-    pii curPos;
-    bool visit[n][m] = {0}; 
-    int dist[n][m] = {0};
+	q.push(goal);
+	board[goal.second][goal.first] = 0;
 
-    q.push(goal);
-    dist[goal.second][goal.first] = now;
+	while (!q.empty()) {
+		auto [curX, curY] = q.front();
+		q.pop();
 
-    while (!q.empty()) {
-        curPos = q.front();
-        q.pop();
-
-        for (int i = 0; i < 4; i++) {
-            int newX = curPos.first + dx[i];
-            int newY = curPos.second + dy[i];
-            if (newX < 0 || newX >= m ||newY < 0 || newY >= n || visit[newY][newX]) continue;
-            
-            dist[newY][newX] = now;
-            visit[newY][newX] = true;
-            q.push({newX, newY});
-        }
-        now++;
-    }
-    
-    for (int y = 0; y < n; y++) {
-        for(int x = 0; x < m; x++) {
-            if (dist[y][x] == 0) {
-                if (board[y][x] == 0 || make_pair(x, y) == goal) { // 못가는 곳이거나 목표지점
-                    continue;
-                }
-                else {
-                    dist[y][x] == -1;
-                }
-            }
-        }
-    }
-
-    for (int y = 0; y < n; y++) {
-        for(int x = 0; x < m; x++) {
-            cout << dist[y][x] << ' ';
-        }
-        cout << endl;
-    }
+		for (int i = 0; i < 4; i++) {
+			int newX = curX + dx[i];
+			int newY = curY + dy[i];
+			if (newX < 0 || newX >= m || // x축 범위 밖
+				newY < 0 || newY >= n || // y축 범위 밖
+				board[newY][newX] >= 0) continue; // 이미 방문한 곳
+			
+			board[newY][newX] = board[curY][curX] + 1;
+			q.push({newX, newY});
+		}
+	}
+	for(auto& row : board) {
+		for (int& x : row) cout << x << ' ';
+		cout << endl;
+	}
 }
 
 int main () {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    input();
-    sol();
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	input();
+	sol();
 }
