f98c4c25d8a48a547e2b7911763679ce234b625a
jin3811 <hjwoo1028@naver.com>
2025-03-09 06:59:18 +0900

    머지소트트리 구현체 추가

--- template/세그트리.cpp
+++ template/세그트리.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#define ALL(X) X.begin(), X.end()
 
 template<class T>
 class SegTree {
@@ -84,5 +85,55 @@ public:
 
 	void print() { 
 		for (int i = 1; i <= len; i++) cout << tree[i] << ' ';
+	}
+};
+
+/**
+ * Merge Sort Tree
+ * 
+ * 세그먼트 트리에 머지소트를 결합한 자료구조
+ */
+template<class T>
+class MSTree {
+	size_t len, elemCnt;
+	vector<vector<T>> tree;
+
+	int _range_greater(int st, int ed, int cur, int left, int right, int k) {
+		if (left > ed || right < st) return 0;
+
+		// 범위 내에 있다면
+		if (left <= st && ed <= right) {
+			return tree[cur].end() - upper_bound(tree[cur].begin(), tree[cur].end(), k); 
+		}
+
+		// flag.아니면 서브트리 확인
+		int mid = (st + ed) >> 1;
+		return _range_greater(st, mid, cur * 2, left, right, k) + _range_greater(mid + 1, ed, cur * 2 + 1, left, right, k);
+	}
+
+public:
+	MSTree(const vector<T>& container) {
+		init(container);
+	}
+
+	MSTree() {}
+
+	void init(const vector<T>& container) {
+		elemCnt = container.size();
+		int height = (int)ceil(log2(elemCnt));
+		len = 1UL << height + 1;
+		tree.resize(len);
+
+		for(int i = 0; i < elemCnt; i++) {
+			tree[len/2 + i].push_back(container[i]);
+		}
+		for(int i = len/2 - 1; i; --i) {
+			tree[i].resize(tree[i * 2].size() + tree[i * 2 + 1].size());
+			merge(ALL(tree[i * 2]), ALL(tree[i * 2 + 1]), tree[i].begin());
+		}
+	}
+
+	int range_greater(int i, int j, int k) {
+		return _range_greater(1, len/2, 1, i, j, k);
 	}
 };
